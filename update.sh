b3a73df7df8e246d644d45a0f24d6877730b9ec5
Breyten Ernsting <bje@dds.nl>
2024-08-07 15:07:45 +0200

    Use token thing.

--- update.sh
+++ update.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
+git pull https://${GITHUB_PERSONAL_TOKEN}@github.com/openstate/amsterdam-subsidies.git master
+
 rm -f message.txt
 touch message.txt
-wget -q -O tmp-subsidies.csv "https://api.data.amsterdam.nl/dcatd/datasets/yvlbMxqPKn1ULw/purls/72c8_AyB5gvJ4Q"
+wget -q -O tmp-subsidies.csv "https://api.data.amsterdam.nl/dcatd/datasets/yvlbMxqPKn1ULw/purls/1"
 csvformat -d ';' -D ',' -e iso-8859-1 tmp-subsidies.csv >tmp-subsidies2.csv
 csvcut -c 1-12 tmp-subsidies2.csv >tmp-subsidies3.csv
 sed '1s/^\xEF\xBB\xBF//;${/^$/d;}' tmp-subsidies3.csv >"subsidies.csv"
@@ -13,5 +15,5 @@ git add subsidies.csv
 git config --global user.email "subsidiebot@bje.dds.nl"
 git config --global user.name "Subsidiebot Amsterdam"
 git commit -F message.txt && \
-              git push https://${GITHUB_PERSONAL_TOKEN}@github.com/openstate/amsterdam-bomen.git master \
+              git push -q https://${GITHUB_PERSONAL_TOKEN}@github.com/openstate/amsterdam-subsidies.git master \
               || true
